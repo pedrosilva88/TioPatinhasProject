5f123bb963172dbac452be7c99c3e1a05c046425
Pedro Silva <pedromiguelsilva88@gmail.com>
2021-03-01 22:02:15 +0000

    logic to create a scanner

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,3 +1,33 @@
+import os, sys
+import csv
+
 class Scanner:
-    def OPGRetailers(self):
-        return ['BIMI', 'COST', 'WMT', 'TJX', 'TGT']
+    tickersDownloaded: [str]
+    tickers: [str]
+
+    def __init__(self):
+        self.tickersDownloaded = []
+        self.tickers = []
+
+    def getOPGRetailers(self):
+        modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+        datapath = os.path.join(modpath, 'scanner/Data/CSV/OPG_Retails.csv')
+        tickers = []
+        
+        with open(datapath) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    tickers.append(row[0])
+                    line_count += 1
+        
+        self.tickersDownloaded = tickers
+        self.tickers = tickers
+        
+        self.tickersDownloaded = ['WMT']
+        self.tickers = ['WMT']
+
+        #return ['BIMI', 'COST', 'WMT', 'TJX', 'TGT']
