24bcf26de0b9bae245c4be4fd893bb38492bb8a3
Pedro Silva <pedromiguelsilva88@gmail.com>
2021-03-03 23:18:07 +0000

    portfolio logic updated

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,23 +1,108 @@
+from datetime import datetime
 from ib_insync import *
-from order import Order, StockPosition
+from order import Order, OrderType, OrderState, OrderExecutionType, StockPosition
 
 class Portfolio:
     cashBalance: float
     positions: [StockPosition]
     orders: [Order]
 
+    @property
+    def cashAvailable(self):
+        return max(self.cashBalance - 400, 0)
+
     def __init__(self):
         self.positions = []
-        self.orders = []
+        self.trades = []
         self.cashBalance = 0
+        self.totalCashBalance = 0
+        self.totalCashBalanceLastUpdate = None
 
     def updatePortfolio(self, ib: IB):
-        self.positions = ib.positions()
-        self.orders = ib.openOrders()
+        self.positions = self.parsePositons(ib)
+        self.orders = self.parseOrders(ib)
+        
         accountValues: [AccountValue] = ib.accountValues()
         account = [d for d in accountValues if d.tag == "AvailableFunds"].pop()
         self.cashBalance = account.value
-        print(self.cashBalance)
-        print(self.positions)
-        print(self.orders)
+
+        currentDatetime = datetime.now()
+        if (not self.totalCashBalanceLastUpdate or currentDatetime.date != self.totalCashBalanceLastUpdate.date):
+            print("Total Cash: %s" % account.value)
+            print(currentDatetime)
+            self.totalCashBalance = account.value
+            self.totalCashBalanceLastUpdate = currentDatetime
+
+    def parsePositons(self, ib: IB):
+        items = ib.positions()
+        list = []
+        for item in items:
+            type = OrderType.Long if item.position > 0 else OrderType.Short
+            position = StockPosition(item.contract.symbol, item.avgCost, item.position, type)
+            list.append(position)
+        
+        return list
+
+    def parseOrders(self, ib: IB):
+        items = ib.trades()
+        subItems = self.filterSubOrders(items)
+        list: [Order] = []
+        for item in items:
+            if (item.orderStatus.status == OrderState.Submitted.value and not item.order.ocaGroup):
+                type = OrderType.Long if item.order.totalQuantity > 0 else OrderType.Short
+                profitPrice, stopLossPrice = self.parseSubOrders(item.order.permId, type, subItems)
+
+                executionType = OrderExecutionType.LimitOrder if item.order.orderType == "LMT" else OrderExecutionType.MarketPrice
+                order = Order(type, item.contract.symbol, item.order.totalQuantity, item.order.lmtPrice, executionType, profitPrice, stopLossPrice)
+                list.append(order)
+        return list
+
+    def filterSubOrders(self, orders: [Trade]):
+        list = []
+        for item in orders:
+            if item.order.ocaGroup:
+                list.append(item)
+        return list
+
+    def parseSubOrders(self, permId: int, type: OrderType, subOrders: [Trade]):
+        profitPrice = None
+        stopLossPrice = None
+        for item in subOrders:
+            if int(item.order.ocaGroup) == permId:
+                if item.order.orderType == 'STP':
+                    stopLossPrice = item.order.auxPrice
+                else:
+                    profitPrice = item.order.lmtPrice
+        return profitPrice, stopLossPrice
+
+    def getPosition(self, ticker: Ticker):
+        for position in self.positions:
+            if position.ticker == ticker.contract.symbol:
+                return position
+        return None
+    
+    def getOrder(self, ticker: Ticker):
+        for order in self.orders:
+            if order.ticker == ticker.contract.symbol:
+                return order
+        return None
+
+    def createOrder(self, ib: IB, order: Order):
+        if len(self.positions) >= 3:
+            return
+        stock = Stock(order.ticker, "SMART", "USD")
+        type = "BUY" if order.type == OrderType.Long else "SELL"
+        ib.bracketOrder(type, order.size, order.price, order.takeProfitPrice, order.stopLossPrice)
+
+            #      List<Order> bracket = OrderSamples.BracketOrder(nextOrderId++, "BUY", 100, 30, 40, 20);
+            # foreach (Order o in bracket)
+            #     client.placeOrder(o.OrderId, ContractSamples.EuropeanStock(), o);
+    
+    def updateOrder(self, ib: IB, order: Order):
+        return None
+
+    def cancelOrder(self, ib: IB, order: Order):
+        return None
+
+
 
--- portfolio/portfolio_doc.py
+++ portfolio/portfolio_doc.py
@@ -1,3 +1,19 @@
+# Deixar de ter MarketOrders e passar a ter LmtPrice Order
+
+# Atualizar o LmtPrice de uma Order olhando para o LastBid & LastAsk
+
+# Ter uma lógica para escolher o ECN em vez de ser o SMART. ne que seja para já o BYX
+
+# Usar o cancelMktData para deixar de escutar uma derterminada stock
+
+# Stock(ticker, "SMART", "USD") Esta parte do "SMART" e "USD" tem que ser mais dinamica
+
+
+
+
+
+# 
+# 
 # ib.reqPositions()
 # ib.reqPositionsAsync
 # cancelPositions
@@ -8,7 +24,7 @@
 ### ib.positions() ###
 [Position(account='DU3352788', contract=Stock(conId=75197507, symbol='EXPR', exchange='NYSE', currency='USD', localSymbol='EXPR', tradingClass='EXPR'), position=5.0, avgCost=2.828), Position(account='DU3352788', contract=Stock(conId=47527197, symbol='EVK', exchange='NASDAQ', currency='USD', localSymbol='EVK', tradingClass='NMS'), position=-5.0, avgCost=3.2541642)]
 
-
+########## cancelMktData ###########
 
 # ib.reqAccountSummaryAsync
 # ib.reqAccountSummary
@@ -27,6 +43,53 @@
 # ib.reqOpenOrdersAsync
 # placeOrder
 # cancelOrder
+
+[Trade(contract=Stock(conId=81330131, symbol='KMF', right='?', exchange='SMART', currency='USD', localSymbol='KMF', tradingClass='KMF'), 
+       order=Order(permId=852390126, action='SELL', totalQuantity=4.0, orderType='LMT', 
+                     lmtPrice=7.89, auxPrice=0.0, tif='DAY', ocaType=3, orderRef='MktDepth', 
+                     trailStopPrice=5.34, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', 
+                     referencePriceType=0, account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), 
+       orderStatus=OrderStatus(orderId=0, status='Submitted', filled=0.0, remaining=4.0, avgFillPrice=0.0, 
+                                   permId=852390126, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='', mktCapPrice=0.0), 
+       fills=[], 
+       log=[TradeLogEntry(time=datetime.datetime(2021, 3, 2, 17, 59, 8, 722816, tzinfo=datetime.timezone.utc), status='Submitted', message='')]), 
+Trade(contract=Stock(conId=12442, symbol='SAN', right='?', exchange='SMART', currency='USD', localSymbol='SAN', tradingClass='SAN'), 
+       order=Order(permId=852390134, action='BUY', totalQuantity=5.0, orderType='LMT', lmtPrice=1.8, auxPrice=0.0, tif='DAY', ocaType=3, 
+                     orderRef='MktDepth', trailStopPrice=4.5, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', 
+                     referencePriceType=0, account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), 
+       orderStatus=OrderStatus(orderId=0, status='Submitted', filled=0.0, remaining=5.0, avgFillPrice=0.0, permId=852390134, parentId=0, lastFillPrice=0.0, clientId=0, 
+                                   whyHeld='', mktCapPrice=0.0), 
+       fills=[], 
+       log=[TradeLogEntry(time=datetime.datetime(2021, 3, 2, 17, 59, 8, 723549, tzinfo=datetime.timezone.utc), status='Submitted', message='')])]
+
+
+
+[Trade(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+       order=Order(permId=1438599791, action='BUY', orderType='MKT', lmtPrice=6.078, auxPrice=0.0, tif='DAY', ocaType=3, orderRef='MktDepth', trailStopPrice=7.078, openClose='', 
+                     volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', clearingIntent='IB', cashQty=0.0, dontUseAutoPriceForHedge=True, 
+                     filledQuantity=4.0, refFuturesConId=2147483647, shareholder='Not an insider or substantial shareholder'), 
+       orderStatus=OrderStatus(orderId=0, status='Filled', filled=0, remaining=0, avgFillPrice=0.0, permId=0, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='', mktCapPrice=0.0), 
+       fills=[Fill(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+                     execution=Execution(execId='0000e859.603f1a4b.01.01', time=datetime.datetime(2021, 3, 3, 8, 49, 29, tzinfo=datetime.timezone.utc), acctNumber='DU3352788', 
+                                          exchange='LSEIOB1', side='BOT', shares=4.0, price=6.074, permId=1438599791, clientId=0, orderId=0, liquidation=0, cumQty=4.0, avgPrice=6.074, 
+                                          orderRef='MktDepth', evRule='', evMultiplier=0.0, modelCode='', lastLiquidity=2), 
+                     commissionReport=CommissionReport(execId='0000e859.603f1a4b.01.01', commission=5.0, currency='USD', realizedPNL=0.0, yield_=0.0, yieldRedemptionDate=0), 
+                     time=datetime.datetime(2021, 3, 3, 8, 49, 29, tzinfo=datetime.timezone.utc))], 
+       log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 8, 49, 29, tzinfo=datetime.timezone.utc), status='Filled', message='Fill 4.0@6.074')]), 
+
+Trade(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+       order=Order(permId=1438599793, action='SELL', totalQuantity=4.0, orderType='LMT', lmtPrice=9.566, auxPrice=0.0, tif='DAY', ocaGroup='1438599791', ocaType=3, orderRef='MktDepth', 
+                     trailStopPrice=7.078, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', 
+                     clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), 
+       orderStatus=OrderStatus(orderId=0, status='PreSubmitted', filled=0.0, remaining=4.0, avgFillPrice=0.0, permId=1438599793, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='', mktCapPrice=0.0), 
+       fills=[], 
+       log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 8, 52, 23, 196144, tzinfo=datetime.timezone.utc), status='PreSubmitted', message='')]), 
+Trade(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+       order=Order(permId=1438599792, action='SELL', totalQuantity=4.0, orderType='STP', lmtPrice=0.0, auxPrice=2.1, tif='DAY', ocaGroup='1438599791', ocaType=3, orderRef='MktDepth', 
+                     trailStopPrice=2.1, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, 
+                     account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), 
+       orderStatus=OrderStatus(orderId=0, status='PreSubmitted', filled=0.0, remaining=4.0, avgFillPrice=0.0, permId=1438599792, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='trigger', mktCapPrice=0.0), fills=[], log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 8, 52, 23, 196889, tzinfo=datetime.timezone.utc), status='PreSubmitted', message='')])]
+
 
 ### ib.reqAllOpenOrders ###
 [Order(permId=1038919788, 
@@ -155,3 +218,104 @@
         AccountValue(account='DU3352788', tag='WarrantValue', value='0.00', currency='BASE', modelCode=''), 
         AccountValue(account='DU3352788', tag='WarrantValue', value='0.00', currency='EUR', modelCode=''), 
         AccountValue(account='DU3352788', tag='WarrantValue', value='0.00', currency='USD', modelCode='')]
+
+
+
+
+        [Trade(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+              order=Order(permId=1438599791, action='BUY', orderType='MKT', lmtPrice=6.078, auxPrice=0.0, tif='DAY', ocaType=3, orderRef='MktDepth', 
+                            trailStopPrice=7.078, openClose='', volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', 
+                            clearingIntent='IB', cashQty=0.0, dontUseAutoPriceForHedge=True, filledQuantity=4.0, refFuturesConId=2147483647, shareholder='Not an insider or substantial shareholder'), 
+              orderStatus=OrderStatus(orderId=0, status='Filled', filled=0, remaining=0, avgFillPrice=0.0, permId=0, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='', mktCapPrice=0.0), 
+              fills=[Fill(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+                            execution=Execution(execId='0000e859.603f1a4b.01.01', time=datetime.datetime(2021, 3, 3, 8, 49, 29, tzinfo=datetime.timezone.utc), 
+                                                 acctNumber='DU3352788', exchange='LSEIOB1', side='BOT', shares=4.0, price=6.074, permId=1438599791, clientId=0, orderId=0, liquidation=0, cumQty=4.0, 
+                                                 avgPrice=6.074, orderRef='MktDepth', evRule='', evMultiplier=0.0, modelCode='', lastLiquidity=2), 
+                            commissionReport=CommissionReport(execId='0000e859.603f1a4b.01.01', commission=5.0, currency='USD', realizedPNL=0.0, yield_=0.0, yieldRedemptionDate=0), 
+                            time=datetime.datetime(2021, 3, 3, 8, 49, 29, tzinfo=datetime.timezone.utc))], 
+              log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 8, 49, 29, tzinfo=datetime.timezone.utc), status='Filled', message='Fill 4.0@6.074')]), 
+       Trade(contract=Stock(conId=48832907, symbol='CABK', right='?', exchange='SMART', currency='EUR', localSymbol='CABK', tradingClass='CABK'), 
+              order=Order(permId=1438599821, action='SELL', totalQuantity=5.0, orderType='LMT', lmtPrice=8.0, auxPrice=0.0, tif='DAY', ocaType=3, orderRef='MktDepth', trailStopPrice=1.545, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), 
+              orderStatus=OrderStatus(orderId=0, status='Submitted', filled=0.0, remaining=5.0, avgFillPrice=0.0, permId=1438599821, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='', mktCapPrice=0.0), fills=[], log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 10, 1, 49, 980147, tzinfo=datetime.timezone.utc), status='Submitted', message='')]), 
+       Trade(contract=Stock(conId=48832907, symbol='CABK', right='?', exchange='SMART', currency='EUR', localSymbol='CABK', tradingClass='CABK'), 
+              order=Order(permId=1438599829, action='BUY', totalQuantity=5.0, orderType='LMT', lmtPrice=1.2, auxPrice=0.0, tif='DAY', ocaGroup='1438599821', ocaType=3, orderRef='MktDepth', trailStopPrice=1.545, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), orderStatus=OrderStatus(orderId=0, status='PreSubmitted', filled=0.0, remaining=5.0, avgFillPrice=0.0, permId=1438599829, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='child', mktCapPrice=0.0), fills=[], log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 10, 1, 49, 980829, tzinfo=datetime.timezone.utc), status='PreSubmitted', message='')]), 
+       Trade(contract=Stock(conId=48832907, symbol='CABK', right='?', exchange='SMART', currency='EUR', localSymbol='CABK', tradingClass='CABK'), 
+              order=Order(permId=1438599826, action='BUY', totalQuantity=5.0, orderType='STP', lmtPrice=0.0, auxPrice=9.0, tif='DAY', ocaGroup='1438599821', ocaType=3, orderRef='MktDepth', trailStopPrice=9.0, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), orderStatus=OrderStatus(orderId=0, status='PreSubmitted', filled=0.0, remaining=5.0, avgFillPrice=0.0, permId=1438599826, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='child,trigger', mktCapPrice=0.0), fills=[], log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 10, 1, 49, 980829, tzinfo=datetime.timezone.utc), status='PreSubmitted', message='')]), 
+       
+
+
+
+
+
+
+
+
+       Trade(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+              order=Order(permId=1438599793, action='SELL', totalQuantity=4.0, orderType='LMT', lmtPrice=9.566, auxPrice=0.0, tif='DAY', ocaGroup='1438599791', ocaType=3, orderRef='MktDepth', trailStopPrice=7.078, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), orderStatus=OrderStatus(orderId=0, status='PreSubmitted', filled=0.0, remaining=4.0, avgFillPrice=0.0, permId=1438599793, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='', mktCapPrice=0.0), fills=[], log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 10, 1, 49, 980829, tzinfo=datetime.timezone.utc), status='PreSubmitted', message='')]), 
+       Trade(contract=Stock(conId=27021698, symbol='OGZD', right='?', exchange='SMART', currency='USD', localSymbol='OGZD', tradingClass='OGZD'), 
+              order=Order(permId=1438599792, action='SELL', totalQuantity=4.0, orderType='STP', lmtPrice=0.0, auxPrice=2.1, tif='DAY', ocaGroup='1438599791', ocaType=3, orderRef='MktDepth', trailStopPrice=2.1, openClose='', eTradeOnly=False, firmQuoteOnly=False, volatilityType=0, deltaNeutralOrderType='None', referencePriceType=0, account='DU3352788', clearingIntent='IB', adjustedOrderType='None', cashQty=0.0, dontUseAutoPriceForHedge=True), orderStatus=OrderStatus(orderId=0, status='PreSubmitted', filled=0.0, remaining=4.0, avgFillPrice=0.0, permId=1438599792, parentId=0, lastFillPrice=0.0, clientId=0, whyHeld='trigger', mktCapPrice=0.0), fills=[], log=[TradeLogEntry(time=datetime.datetime(2021, 3, 3, 10, 1, 49, 980829, tzinfo=datetime.timezone.utc), status='PreSubmitted', message='')])]
+
+
+
+
+2021-03-03 14:30:39.224173+00:00
+Type(OrderType.Short) Size(2) Price(119.45) ProfitPrice(119.28) StopLoss(125.42)
+Result for GME: Sell 💚
+
+2021-03-03 14:30:39.224173+00:00
+The GAP is poor or don't exist. Do nothing! 1.38 - -0.21 ( 15.23 - 15.44 )
+Result for M: DoNothing 💚
+
+2021-03-03 14:30:39.224173+00:00
+The GAP is poor or don't exist. Do nothing! 0.75 - -0.02 ( 2.65 - 2.67 )
+Result for EXPR: DoNothing 💚
+
+2021-03-03 14:30:34.040289+00:00
+The GAP is poor or don't exist. Do nothing! 1.23 - -0.34 ( 27.73 - 28.07 )
+Result for BBBY: DoNothing 💚
+
+2021-03-03 14:30:39.222953+00:00
+The GAP is poor or don't exist. Do nothing! 0.98 - 0.13 ( 13.23 - 13.10 )
+Result for CLNE: DoNothing 💚
+
+2021-03-03 14:30:39.224173+00:00
+The GAP is poor or don't exist. Do nothing! 0.55 - 0.71 ( 130.11 - 129.40 )
+Result for WMT: DoNothing 💚
+
+2021-03-03 14:30:39.224173+00:00
+The GAP is poor or don't exist. Do nothing! 0.04 - 0.01 ( 25.73 - 25.72 )
+Result for GPS: DoNothing 💚
+
+2021-03-03 14:30:37.274656+00:00
+The GAP is poor or don't exist. Do nothing! 0.63 - 0.30 ( 47.44 - 47.14 )
+Result for WBA: DoNothing 💚
+
+2021-03-03 14:30:39.224173+00:00
+The GAP is poor or don't exist. Do nothing! 0.77 - 0.51 ( 66.21 - 65.70 )
+Result for TJX: DoNothing 💚
+
+2021-03-03 14:30:37.274656+00:00
+The GAP is poor or don't exist. Do nothing! 0.81 - 0.08 ( 9.91 - 9.83 )
+Result for LOTZ: DoNothing 💚
+
+2021-03-03 14:30:39.224173+00:00
+The GAP is poor or don't exist. Do nothing! 0.24 - 0.26 ( 107.20 - 106.94 )
+Result for SBUX: DoNothing 💚
+
+2021-03-03 14:30:39.224173+00:00
+The GAP is poor or don't exist. Do nothing! 22.48 - -4.05 ( 18.02 - 22.07 )
+Result for MIK: DoNothing 💚
+
+2021-03-03 14:30:39.224173+00:00
+Type(OrderType.Long) Size(8) Price(35.40) ProfitPrice(36.01) StopLoss(33.63)
+Result for JWN: Buy 💔 (Earnings) # ainda nao terminou
+
+2021-03-03 14:30:41.147125+00:00
+Type(OrderType.Short) Size(13) Price(21.93) ProfitPrice(22.85) StopLoss(23.03)
+Result for VUZI: Sell 💚
+
+2021-03-03 14:30:43.152379+00:00
+The GAP is poor or don't exist. Do nothing! 0.84 - -0.48 ( 57.36 - 57.84 )
+Result for KSS: DoNothing 💚
+
+--------
