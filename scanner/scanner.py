90181093ac7e05c57984c87308ed9572c8570aa8
Pedro Silva <pedromiguelsilva88@gmail.com>
2021-03-04 18:04:51 +0000

    Update Scanner code

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,9 +1,10 @@
 import os, sys
 import csv
+from order import Ticker
 
 class Scanner:
-    tickersDownloaded: [str]
-    tickers: [str]
+    tickersDownloaded: [Ticker]
+    tickers: [Ticker]
 
     def __init__(self):
         self.tickersDownloaded = []
@@ -11,23 +12,19 @@ class Scanner:
 
     def getOPGRetailers(self):
         modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-        datapath = os.path.join(modpath, 'scanner/Data/CSV/OPG_Retails.csv')
+        datapath = os.path.join(modpath, 'scanner/Data/CSV/OPG_Retails_SortFromBackTest.csv')
+        #datapath = os.path.join(modpath, 'scanner/Data/CSV/OPG_Retails.csv')
         tickers = []
         
         with open(datapath) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
             for row in csv_reader:
-                if line_count == 0:
-                    line_count += 1
-                else:
-                    tickers.append(row[0])
-                    line_count += 1
+                if line_count > 0:
+                    tickers.append(Ticker(row[0], None))
         
         self.tickersDownloaded = tickers[:15]
         self.tickers = tickers[:15]
-        
-        # self.tickersDownloaded = ['WMT']
-        # self.tickers = ['WMT']
 
-        #return ['BIMI', 'COST', 'WMT', 'TJX', 'TGT']
+    def getTicker(self, symbol: str):
+        ticker = [d for d in self.tickers if d.symbol == symbol].pop()
+        return ticker
