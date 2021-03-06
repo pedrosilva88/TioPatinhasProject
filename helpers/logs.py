965514ec351186e07565de95a5697f62a9c2c9c3
Pedro Silva <pedromiguelsilva88@gmail.com>
2021-03-06 21:12:11 +0000

    Logs updated

--- helpers/logs.py
+++ helpers/logs.py
@@ -1,13 +1,20 @@
 from strategy import StrategyData, StrategyResult, StrategyResultType
 
 def logExecutionTicker(data: StrategyData, result: StrategyResult):
-    print("⭐️[%i/%i %i:%i:%i] Ticker %s: %s / Last(%.2f) Open(%.2f) Close(%.2f) ⭐️" % (data.datetime.day, 
-                                                                                    data.datetime.month, 
-                                                                                    data.datetime.hour, 
-                                                                                    data.datetime.minute, 
-                                                                                    data.datetime.second, 
-                                                                                    data.ticker.symbol,
+    print("⭐️[%i/%i %i:%i:%i] Ticker %s: %s / Last(%.2f) Open(%.2f) Close(%.2f) ⭐️" % (data.ticker.time.day, 
+                                                                                    data.ticker.time.month, 
+                                                                                    data.ticker.time.hour, 
+                                                                                    data.ticker.time.minute, 
+                                                                                    data.ticker.time.second, 
+                                                                                    data.ticker.contract.symbol,
                                                                                     result.type,
-                                                                                    data.lastPrice,
-                                                                                    data.openPrice,
-                                                                                    data.ystdClosePrice))
+                                                                                    data.ticker.last,
+                                                                                    data.ticker.open,
+                                                                                    data.ticker.close))
+
+def logInitTioPatinhas():
+    print("\t🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆\n")
+    print("\t🦆\t\t\t   🦆\n")
+    print("\t🦆   Running Tio Patinhas  🦆\n")
+    print("\t🦆\t\t\t   🦆\n")
+    print("\t🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆\n")
